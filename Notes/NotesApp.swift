cc5ae91ac23fd82ed36c5cf7b27870ff9ea8ad12
Ilyusha K <ilyusha_k@icloud.com>
2021-02-12 20:02:37 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Ilya Kulinkovich on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
